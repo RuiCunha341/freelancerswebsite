8e30beec27ef4184ce0bb6a9ea5ce433ff83ae19
RuiCunha341 <rui.cunha@linkwithus.pt>
2024-07-16 19:15:51 +0100

    feat: HomePage test

--- src/containers/Menu/index.tsx
+++ src/containers/Menu/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './styles.scss';
+import Button from 'components/Button';
 import { IMenu } from './interface';
 
 function Menu({ reference }: IMenu) {
     return (
         <div className='website-menu' ref={reference}>
-            <div className='website-menu__left-side'>Menu Left-Side</div>
-            <div className='website-menu__right-side'>Menu Right-Side</div>
+            <div className='website-menu__left-side'>Logo</div>
+            <div className='website-menu__right-side'>
+                <Button text='Contact Us' />
+            </div>
         </div>
     );
 }
