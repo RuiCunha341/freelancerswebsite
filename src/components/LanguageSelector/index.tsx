a4bfa01f0a36fb1c6c1d58901002ceb0ac964d3f
RuiCunha341 <rui.cunha@linkwithus.pt>
2024-07-23 12:22:42 +0100

    feat: HomePage test

---
+++ src/components/LanguageSelector/index.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useRef, useState } from 'react';
+import './styles.scss';
+import { useTranslation } from 'react-i18next';
+import classNames from 'classnames';
+import { LanguageEnum } from 'enums/languages';
+import DownArrowIcon from 'assets/icons/DownArrowIcon';
+import useClickOutside from 'hooks/ClickOutside';
+
+function LanguageSelector() {
+    // language => "en" or "pt"
+    const { i18n } = useTranslation();
+    const [dropdownVisible, setDropdownVisible] = useState(false);
+    const [language, setLanguage] = useState<LanguageEnum>(
+        LanguageEnum.English,
+    );
+    const dropdownContainerRef = useRef<HTMLDivElement>(null);
+
+    /** Function used to toggle on and off the dropdown content visibility */
+    const toggleDropdown = () => {
+        setDropdownVisible(!dropdownVisible);
+    };
+
+    /** Force close the dropdown menu when the user clicks outside of its container */
+    useClickOutside(dropdownContainerRef, () => setDropdownVisible(false));
+
+    useEffect(() => {
+        // Get the current language from the html element
+        const currentLang = document.documentElement.lang;
+
+        if (currentLang === LanguageEnum.Portuguese)
+            setLanguage(LanguageEnum.Portuguese);
+        // default language is EN
+        setLanguage(LanguageEnum.English);
+    }, []);
+
+    useEffect(() => {
+        i18n.changeLanguage(language);
+    }, [language]);
+
+    return (
+        <div className='language-selector-container' ref={dropdownContainerRef}>
+            <button
+                type='button'
+                onClick={toggleDropdown}
+                className='language-selector-container__button'
+            >
+                <span>{language.toUpperCase()}</span>
+                <DownArrowIcon />
+            </button>
+            <div
+                className={classNames('language-selector-container__dropdown', {
+                    active: dropdownVisible,
+                })}
+            >
+                {language.toUpperCase() !==
+                    LanguageEnum.English.toUpperCase() && (
+                    <button
+                        type='button'
+                        onClick={() => {
+                            setLanguage(LanguageEnum.English);
+                            toggleDropdown();
+                        }}
+                        className='language-selector-container__dropdown__button'
+                    >
+                        {LanguageEnum.English.toUpperCase()}
+                    </button>
+                )}
+                {language.toUpperCase() !==
+                    LanguageEnum.Portuguese.toUpperCase() && (
+                    <button
+                        type='button'
+                        className='language-selector-container__dropdown__button'
+                        onClick={() => {
+                            setLanguage(LanguageEnum.Portuguese);
+                            toggleDropdown();
+                        }}
+                    >
+                        {LanguageEnum.Portuguese.toUpperCase()}
+                    </button>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default LanguageSelector;
